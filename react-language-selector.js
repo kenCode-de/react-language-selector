585f74c9c45fa7f66ea781ebc83e5092a39e37ba
poqdavid <poq-david@live.com>
2015-10-09 02:30:39 +0330

    prevented extra file download and better xpath support

--- react-language-selector.js
+++ react-language-selector.js
@@ -1,53 +1,83 @@
-var Lang = (function () {
+var Lang;
+Lang = (function () {
 
 	var Lang = function (defaultLangfile, defaultLang) {
 		var self = this;
 
 		if (typeof(Storage) !== "undefined") {
 			self.set_load_lang(self.getsetting('lang', defaultLang), self.getsetting('langfile', defaultLangfile), true);
-			//localStorage.setItem('langfile', self.getsetting('langfile', defaultLangfile));
 		} else {
 			self.set_load_lang(defaultLang, defaultLangfile, true);
 		}
-
-		//window.onload = function () {
-		//if (typeof(Storage) !== "undefined") {
-		//	self.setmyLang(self.getsetting('lang', defaultLang));
-		//} else {
-		//	self.setmyLang(defaultLang);
-		//}
-		//};
 	};
 
-	//Lang.prototype.addedpacks = {};
+	Array.prototype.contains = function (obj) {
+		var i = this.length;
+		while (i--) {
+			if (this[i] === obj) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	Lang.prototype.pack = {};
+	Lang.prototype.loadedfiles = [];
+
 	Lang.prototype.loadLangPack = function (callback, myLangFile, isfirst) {
 		var self = this;
-		//TODO: check if the json object is added before.
-		self.loadJSON(function (response) {
-			var result = JSON.parse(response);
-			if (isfirst == true) {
-				self.pack = result;
-			} else {
-				for (var i = 0; i < result.length; i++) {
-					self.pack.push(result[i]);
-					//var jsonStr = JSON.stringify(self.pack);
+		if (!self.loadedfiles.contains(myLangFile)) {
+			self.loadJSON(function (response) {
+				var result = JSON.parse(response);
+				if (isfirst == true) {
+					self.pack = result;
+				} else {
+					for (var i = 0; i < result.length; i++) {
+						self.pack.push(result[i]);
+					}
 				}
-			}
+				localStorage.setItem('langfile', myLangFile);
+				callback();
+			}, myLangFile);
+		} else {
 			localStorage.setItem('langfile', myLangFile);
 			callback();
-		}, myLangFile);
+		}
 	};
 
 	Lang.prototype.set_lang = function (myLang) {
 		var self = this;
-
+		var num = 0;
+		var oldelem;
 		for (var i = 0; i < self.pack.length; i++) {
 			if (self.pack[i].lang == myLang) {
-				var child1 = React.createElement('lable', null, self.pack[i].text);
-				React.render(child1, document.evaluate(self.pack[i].elem, document, null, 9, null).singleNodeValue);
+
+				noderesult = document.evaluate(self.pack[i].elem, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+				var thisNode = noderesult.iterateNext();
+
+				if (i != 0) {
+					if (self.pack[i].elem == oldelem) {
+						num++;
+					}
+					if (num != 0) {
+						for (var x = 0; x < num; x++) {
+							thisNode = noderesult.iterateNext();
+						}
+					} else {}
+
+					var child1 = React.createElement('lable', null, self.pack[i].text);
+
+					React.render(child1, thisNode);
+				} else {
+					var child1 = React.createElement('lable', null, self.pack[i].text);
+
+					React.render(child1, thisNode);
+				}
+				oldelem = self.pack[i].elem;
 			}
 		}
+
 		localStorage.setItem('lang', myLang);
 	};
 
@@ -56,11 +86,10 @@ var Lang = (function () {
 
 		self.loadLangPack(function () {
 			if (isfirst == true) {
-				document.onreadystatechange = function () {
-					var state = document.readyState
-						if (state == 'complete') {
-							self.set_lang(myLang);
-						}
+				var state = document.readyState;
+
+				if (state == 'complete') {
+					self.set_lang(myLang);
 				}
 			} else {
 				self.set_lang(myLang);
@@ -74,10 +103,13 @@ var Lang = (function () {
 	};
 
 	Lang.prototype.loadJSON = function (callback, myfile) {
+		var self = this;
 		var xmlhttp = new XMLHttpRequest();
 
 		xmlhttp.onreadystatechange = function () {
 			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+				self.loadedfiles.push(myfile);
 				callback(xmlhttp.responseText);
 			}
 		};
